e99d1c8bba4b47de4f32f9f21d82c8dcfaa6a60f
Pete Ware <ware@peteware.com>
2015-01-16 21:27:36 -0500

    Fixing server-start and packag archive

--- main-init.el
+++ main-init.el
@@ -54,9 +54,9 @@
             '(("no_proxy" . "^\\(localhost\\|10.*\\)")
               ("http" . "devproxy.bloomberg.com:82")
               ("https" . "devproxy.bloomberg.com:82"))))
+    (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
     (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
     (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
-    (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
     (package-initialize)
     (use-package pw-pkg-install
       :init
